c329e959603f2a71cbc098666426d85cf8dc1875
mohd huzaif <huzaif012@gmail.com>
2021-10-20 18:02:04 +0530

    first folder

---
+++ Assingnment operator/areaOfTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * areaOfTriangle
+ */
+public class areaOfTriangle {
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter breadth:");
+        int breadth = obj.nextInt();
+        System.out.println("Enter height:");
+        int height = obj.nextInt();
+        double areaOfTriangle = 0.5*breadth*height;
+        System.out.println("Area of Triangle:"+areaOfTriangle);
+    }
+}
